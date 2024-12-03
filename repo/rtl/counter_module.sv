6392b1d63e97c1fb56218a8ac110101fa15756d9
selimenur <selimenurozyurt@gmail.com>
2024-12-03 14:45:03 +0000

    Counter module update

--- repo/rtl/counter_module.sv
+++ repo/rtl/counter_module.sv
@@ -1,30 +1,65 @@
+`include "definitons.sv"
+
 module counter_module #(
-    parameter DATA_WIDTH = 16
+    parameter DATA_WIDTH = 32
 )(
     // Interface signals
-    input logic clk,                          // Clock
-    input logic rst,                          // Reset
-    input logic PCSrc,                        // Select signal for MUX 
-    input logic stallF,                       // Enable bit for clk from Hazard Unit
-    input logic [DATA_WIDTH-1:0] PCTarget,    // Branch target address (PC + ImmExt)
+    input logic [2:0] PCSrcE,                        // Select signal for MUX 
+    input logic [DATA_WIDTH-1:0] PCTargetE,    // Branch target address (PC + ImmExt)
     input logic [DATA_WIDTH-1:0] PCPlus4F,     // Incremented PC (PC + 4)
-    output logic [DATA_WIDTH-1:0] PCounterF    // Program Counter after clk
+    input logic [DATA_WIDTH-1:0]    ALUResult,
+    input logic                   eq,
+    output logic [DATA_WIDTH-1:0] pc,    // Program Counter after clk
+    output logic                  Hazard_PCsrc
 );
 
-    // Internal signal for PCnext (MUX output)
-    logic [DATA_WIDTH-1:0] PCnext;
-
-    // MUX logic: Select between PCTarget and PCPlus4 based on PCSrc
-    assign PCnext = PCSrc ? PCTarget : PCPlus4F;
-
-    // Sequential logic to update PCounter on clock edge
-    always_ff @(posedge clk) begin
-        if (rst)
-            PCounterF <= {DATA_WIDTH{1'b0}}; // Reset PCounter to 0
-        else if (!stallF) begin
-            PCounterF <= PCnext; // Update PCounter to the next value, if the clk is enabled            
+    always_comb begin 
+    case(PCsrcE)
+        `NEXT_PC: begin
+            pc = PCPlus4F;
+            Hazard_PCsrc = 0;
         end
-            
-    end
+
+        `EQ_TRUE: begin 
+            if(eq) begin 
+                pc = PCTargetE;
+                Hazard_PCsrc = 1;
+            end
+            else begin 
+                pc = PCPlus4F;
+                Hazard_PCsrc = 0;
+            end
+        end
+
+        `EQ_FALSE: begin 
+            if(!eq) begin 
+                pc = PCTargetE;
+                Hazard_PCsrc = 1;
+            end
+            else begin 
+                pc = PCPlus4F;
+                Hazard_PCsrc = 0;
+            end
+        end
+
+        `UC_JUMP: begin 
+            pc = PCTargetE;
+            Hazard_PCsrc = 1;
+        end
+
+        `JALR: begin 
+            pc = ALUResult;
+            Hazard_PCsrc = 1;
+        end
+        default: begin 
+            pc = 0;
+            Hazard_PCsrc = 0;
+            $display("Unrecognized PCsrcE");
+        end
+    endcase 
+end
+
+
+
 
 endmodule
