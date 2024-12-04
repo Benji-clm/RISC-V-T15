b73413423637a0af0a9e71f0f2e36cf1162e4b4e
Augustin-Briens <augustin.briens23@imperial.ac.uk>
2024-12-04 03:22:37 +0000

    Harmonisation between all modules for same Inputs and Outputs for functional top file

--- repo/rtl/top.sv
+++ repo/rtl/top.sv
@@ -1,16 +1,17 @@
 module top #(
     DATA_WIDTH = 32,
-    ADDRESS_WIDTH = 16
+    ADDRESS_WIDTH = 32,
+    MEMORY_WIDTH = 8
 ) (
     input   logic clk,
     input   logic rst,
+    input   logic trigger,
     output  logic [DATA_WIDTH-1:0] a0    
 );
     assign a0 = 0;
 
 logic [ADDRESS_WIDTH-1:0]   next_pc;
 logic [DATA_WIDTH-1:0]      instr;
-logic [2:0]                 LS_mode;
 
 logic [DATA_WIDTH-1:0]      SrcAE;
 logic [DATA_WIDTH-1:0]      WriteDataE;
@@ -22,42 +23,44 @@ logic [DATA_WIDTH-1:0]      ALUResult;
 // pipeline logic
 
 // Fetch stage
-logic [ADDRESS_WIDTH-1:0]                   pcF;
-logic [ADDRESS_WIDTH-1:0]                   PCPlus4F;
+logic [ADDRESS_WIDTH-1:0]   pcF;
+logic [ADDRESS_WIDTH-1:0]   PCPlus4F;
 
 // Decode stage
-logic [DATA_WIDTH-1:0]   instrD;
-logic                    RegWriteD;
-logic [1:0]              ResultSrcD;
-logic                    MemWriteD;
-logic [2:0]              PCsrcD;
-logic [3:0]              ALUControlD;
-logic                    ALUsrcD;
-logic [2:0]              ImmSrcD;
-logic [DATA_WIDTH-1:0]   rd1;
-logic [DATA_WIDTH-1:0]   rd2;
-logic [DATA_WIDTH-1:0]   pcD;
-logic [4:0]              Rs1D;
-logic [4:0]              Rs2D;
-logic [4:0]              RdD;
-logic [DATA_WIDTH-1:0]   ImmExtD;
-logic [DATA_WIDTH-1:0]   PCPlus4D;
+logic [DATA_WIDTH-1:0]      instrD;
+logic                       RegWriteD;
+logic [1:0]                 ResultSrcD;
+logic                       MemWriteD;
+logic [2:0]                 PCsrcD;
+logic [3:0]                 ALUControlD;
+logic                       ALUsrcD;
+logic [2:0]                 ImmSrcD;
+logic [DATA_WIDTH-1:0]      rd1;
+logic [DATA_WIDTH-1:0]      rd2;
+logic [DATA_WIDTH-1:0]      pcD;
+logic [4:0]                 Rs1D;
+logic [4:0]                 Rs2D;
+logic [4:0]                 RdD;
+logic [DATA_WIDTH-1:0]      ImmExtD;
+logic [DATA_WIDTH-1:0]      PCPlus4D;
+logic [2:0]                 LS_modeD;
 
 // Execute stage
-logic                    RegWriteE;
-logic [1:0]              ResultSrcE;
-logic                    MemWriteE;
-logic [2:0]              PCsrcE;
-logic [3:0]              ALUControlE;
-logic                    ALUsrcE;
-logic [DATA_WIDTH-1:0]   rd1E;
-logic [DATA_WIDTH-1:0]   rd2E;
-logic [DATA_WIDTH-1:0]   pcE;
-logic [4:0]              Rs1E;
-logic [4:0]              Rs2E;
-logic [4:0]              RdE;
-logic [DATA_WIDTH-1:0]   ImmExtE;
-logic [DATA_WIDTH-1:0]   PCPlus4E;
+logic                       RegWriteE;
+logic [1:0]                 ResultSrcE;
+logic                       MemWriteE;
+logic [2:0]                 PCsrcE;
+logic [3:0]                 ALUControlE;
+logic                       ALUsrcE;
+logic [DATA_WIDTH-1:0]      rd1E;
+logic [DATA_WIDTH-1:0]      rd2E;
+logic [DATA_WIDTH-1:0]      pcE;
+logic [4:0]                 Rs1E;
+logic [4:0]                 Rs2E;
+logic [4:0]                 RdE;
+logic [DATA_WIDTH-1:0]      ImmExtE;
+logic [DATA_WIDTH-1:0]      PCPlus4E;
+logic [2:0]                 LS_modeE;
 
 // Memorystage
 logic                       RegWriteM;
@@ -67,6 +70,7 @@ logic [DATA_WIDTH-1:0]      ALUResultM;
 logic [DATA_WIDTH-1:0]      WriteDataM;
 logic [4:0]                 RdM;
 logic [DATA_WIDTH-1:0]      PCPlus4M;
+logic [2:0]                 LS_modeM;
 
 // WriteBack stage
 logic                       RegWriteW;
@@ -91,25 +95,38 @@ logic                       FlushE;
 logic [1:0]                 ForwardAE;
 logic [1:0]                 ForwardBE;
 
+logic MemReadD;
+logic MemReadE;
+logic MemReadM;
 
 // control unit
 logic [6:0]            op;
 logic [2:0]            funct3;
 logic [6:0]            funct7;
 
-pc #(ADDRESS_WIDTH) pc_counter(
-    .clk(clk),
-    .rst(rst),
-    .branch_PC(pc + ImmOp),
-    .inc_pc(pc + 4),
-    .PCsrc(PCsrc),
-    .next_PC(next_PC)
+logic [31:7]           instr_ext;
+
+assign PCPlus4F = pcF +4;
+
+counter_module #(ADDRESS_WIDTH) pc_counter(
+    .PCsrcE(PCsrcE),
+    .PCPlus4F(PCPlus4F),
+    .PCTargetE(pcE + ImmExtE),
+    .ALUResult(ALUResult),
+    .eq(eq),
+    .Hazard_PCsrc(Hazard_PCsrc),
+    .pc(next_pc)
 );
 
-assign pc = next_PC;
+always_ff @(posedge clk) begin
+    if (!StallF) begin
+        pcF <= rst ? 32'h0 : next_pc;
+    end
+end
 
-i_mem #(DATA_WIDTH, ADDRESS_WIDTH) instr_mem(
-    .pc(pc),
+
+i_mem #(DATA_WIDTH) instr_mem(
+    .pc(pcF),
     .instr(instr)
 );
 
@@ -117,17 +134,17 @@ fetch_decode_pipe #(DATA_WIDTH) pipeline_FD(
     .clk(clk),
 
     // controls
-    .stallD(stallD),
-    .flushD(flushD),
+    .StallD(StallD),
+    .FlushD(FlushD),
 
     // inputs
-    .PCounterF(pcF),
+    .pcF(pcF),
     .instr(instr),
     .PCPlus4F(PCPlus4F),
 
     // outputs
-    .InstrD(instrD),
-    .PCounterD(pcD),
+    .instrD(instrD),
+    .pcD(pcD),
     .PCPlus4D(PCPlus4D)
 );
 
@@ -137,7 +154,7 @@ assign funct7 = instrD[31:25];
 
 control_unit ctrl_unit(
     .op(op),
-    .stall(stall),
+    .stall(StallD),
     .funct3(funct3),
     .funct7(funct7),
     .PCsrc(PCsrcD),
@@ -147,8 +164,8 @@ control_unit ctrl_unit(
     .ALUsrc(ALUsrcD),
     .ImmSrc(ImmSrcD),
     .RegWrite(RegWriteD),
-    .LS_mode(LS_mode),
-    .MemRead(ResultSrcM)
+    .LS_mode(LS_modeD),
+    .MemRead(MemReadD)
 );
 
 assign instr_ext = instrD[31:7];
@@ -163,16 +180,17 @@ assign Rs2D = instrD[24:20];
 assign Rs1D = instrD[19:15];
 assign RdD = instrD[11:7];
 
-regf #(DATA_WIDTH, ADDRESS_WIDTH) registers(
+regf #(DATA_WIDTH) registers(
     .clk(clk),
     .a1(Rs1D),
     .a2(Rs2D),
-    .a3(rdW),
+    .a3(RdW),
     .we3(RegWriteW),
     .wd3(ResultW),
     .rd1(rd1),
     .rd2(rd2),
-    .a0(a0)
+    .a0(a0),
+    .t0(trigger)
 );
 
 decode_execute_pipe #(DATA_WIDTH) pipeline_DE_inst (
@@ -187,6 +205,7 @@ decode_execute_pipe #(DATA_WIDTH) pipeline_DE_inst (
     .PCsrcD(PCsrcD),
     .ALUControlD(ALUControlD),
     .ALUsrcD(ALUsrcD),
+    .MemReadD(MemReadD),
 
     // Register file, extend, and PC inputs
     .rd1(rd1),
@@ -196,7 +215,8 @@ decode_execute_pipe #(DATA_WIDTH) pipeline_DE_inst (
     .Rs1D(Rs1D),
     .Rs2D(Rs2D),
     .RdD(RdD),
-    .ImmExtD(ImmExtD),    
+    .ImmExtD(ImmExtD),
+    .LS_modeD(LS_modeD),  
 
     // Outputs to the next stage
     .RegWriteE(RegWriteE),
@@ -212,7 +232,9 @@ decode_execute_pipe #(DATA_WIDTH) pipeline_DE_inst (
     .Rs1E(Rs1E),
     .Rs2E(Rs2E),
     .RdE(RdE),
-    .ImmExtE(ImmExtE)
+    .ImmExtE(ImmExtE),
+    .LS_modeE(LS_modeE),
+    .MemReadE(MemReadE)
 );
 
 mux3 #(DATA_WIDTH) muxForwardAE(
@@ -247,43 +269,59 @@ alu #(DATA_WIDTH) alu_(
 );
 
 execute_mem_pipe #(DATA_WIDTH) pipeline_EM(
-    .clk(clk),                           
-    .flush(1'b0),                        
+    .clk(clk),                                                  
 
     // Inputs from EX stage
-    .alu_result(ALUResult),              
-    .write_data(WriteDataE),             
-    .daddr(PCPlus4E),                    
-    .mem_write(MemWriteE),               
-    .mem_read(1'b0),                     
-    .funct3(funct3),                     
-    .reg_dest(RdE),                      
+    .ALUResult(ALUResult),              
+    .WriteDataE(WriteDataE),             
+    .PCPlus4E(PCPlus4E),                    
+    .MemWriteE(MemWriteE),                                   
+    .LS_modeE(LS_modeE),                     
+    .RdE(RdE), 
+    .ResultSrcE(ResultSrcE),   
+    .RegWriteE(RegWriteE),                
+    .MemReadE(MemReadE),
 
     // Outputs to MEM stage
-    .mem_alu_result(ALUResultM),         // ALU result to MEM stage
-    .mem_write_data(WriteDataM),         // Write data to MEM stage
-    .mem_daddr(PCPlus4M),                // Data memory address to MEM stage
-    .mem_mem_write(MemWriteM),           // Memory write enable to MEM stage
-    .mem_mem_read(ResultSrcM),           // Memory read enable to MEM stage (mapped from ResultSrcM)
-    .mem_funct3(mem_funct3),                 // Function control (if used, map appropriately)
-    .mem_reg_dest(RdM)                   // Register destination to MEM stage
+    .ALUResultM(ALUResultM),         // ALU result to MEM stage
+    .WriteDataM(WriteDataM),         // Write data to MEM stage
+    .PCPlus4M(PCPlus4M),                // Data memory address to MEM stage
+    .MemWriteM(MemWriteM),           // Memory write enable to MEM stage
+    .ResultSrcM(ResultSrcM),           // Memory read enable to MEM stage (mapped from ResultSrcM)
+    .LS_modeM(LS_modeM),                 // Function control (if used, map appropriately)
+    .RdM(RdM),                   // Register destination to MEM stage
+    .RegWriteM(RegWriteM),
+    .MemReadM(MemReadM)
 );
 
+top_mem top_memory_inst (
+        .clk(clk),                      
+        .we(MemWriteM),            
+        .LS_mode(LS_modeM),
+        .MemRead(MemReadM),                
+        .a(ALUResultM),              
+        .wd(WriteDataM),       
+        .rd(rd)               
+    );
 
 mem_writeback_pipe #(DATA_WIDTH) pipeline_MW_inst(
     .clk(clk),                          // Clock signal
-    .flush(1'b0),                       // Assuming flush is not being used in this context, set to 0 or connect appropriately
 
     // Inputs from MEM stage
-    .mem_alu_result(ALUResultM),        // ALU result from MEM stage
-    .mem_mem_data(PCPlus4M),            // Memory data or equivalent, assuming PCPlus4M is used here as memory data
-    .mem_reg_dest(RdM),                 // Register destination from MEM stage
-    .mem_reg_write(RegWriteM),          // Register write enable from MEM stage
+    .ALUResultM(ALUResultM),        // ALU result from MEM stage
+    .PCPlus4M(PCPlus4M),            // Memory data or equivalent, assuming PCPlus4M is used here as memory data
+    .RdM(RdM),                 // Register destination from MEM stage
+    .RegWriteM(RegWriteM),          // Register write enable from MEM stage
+    .rd(rd),
+    .ResultSrcM(ResultSrcM),
 
     // Outputs to WB stage
-    .wb_data(ALUResultW),               // Data to write back to the register file
-    .wb_reg_dest(RdW),                  // Register destination to WB stage
-    .wb_reg_write(RegWriteW)            // Register write enable to WB stage
+    .ALUResultW(ALUResultW),               // Data to write back to the register file
+    .RdW(RdW),                  // Register destination to WB stage
+    .RegWriteW(RegWriteW),            // Register write enable to WB stage
+    .PCPlus4W(PCPlus4W),
+    .ReadDataW(ReadDataW),
+    .ResultSrcW(ResultSrcW)
 );
 
 mux3 #(DATA_WIDTH) mux3_(
@@ -302,7 +340,7 @@ hazard_unit hazard_unit_(
     .Rs2E(Rs2E),
     .RdE(RdE),
     .Hazard_PCsrc(Hazard_PCsrc),
-    .ResultSrcE(ResultSrcE),
+    .ResultSrcE(ResultSrcE[0]),
     .RdM(RdM),
     .RdW(RdW),
     .RegWriteM(RegWriteM),
@@ -317,15 +355,7 @@ hazard_unit hazard_unit_(
     .ForwardBE(ForwardBE)
 );
 
-top_mem top_memory_inst (
-        .clk(clk),                      
-        .we(we),            
-        .funct3(mem_funct3),                
-        .daddr(ALUResultM),              
-        .wd_data(WriteDataM),       
-        .cache_hit(cache_hit),                      
-        .out(ReadDataW)               
-    );
+
 
 
 
