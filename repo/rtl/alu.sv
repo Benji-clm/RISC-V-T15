6fc02e2185d52ad8efd14be24972a6c3eac80e4e
Augustin-Briens <augustin.briens23@imperial.ac.uk>
2024-12-04 03:15:27 +0000

    Alu finalisation

--- repo/rtl/alu.sv
+++ repo/rtl/alu.sv
@@ -16,27 +16,23 @@ assign op1_signed = ALUop1;
 assign op2_signed = ALUop2;
 
 always_comb begin
-    // Default values for outputs
-    ALUout = 0;
-    eq = 0;
 
     case(ALUctrl)
-        `ALUop_ADD:  ALUout = ALUop1 + ALUop2;                             // Addition
-        `ALUop_SUB:  ALUout = ALUop1 - ALUop2;                             // Subtraction
-        `ALUop_AND:  ALUout = ALUop1 & ALUop2;                             // AND
-        `ALUop_OR:   ALUout = ALUop1 | ALUop2;                             // OR
-        `ALUop_XOR:  ALUout = ALUop1 ^ ALUop2;                             // XOR
-        `ALUop_LSL:  ALUout = ALUop1 << ALUop2;                       // Logical shift left
-        `ALUop_LSR:  ALUout = ALUop1 >> ALUop2;                       // Logical shift right
-        `ALUop_ASR:  ALUout = op1_signed >>> ALUop2;                  // Arithmetic shift right
-        `ALUop_SLT:  ALUout = (op1_signed < op2_signed) ? 1 : 0;           // Set less than (signed)
-        `ALUop_SLTU: ALUout = (ALUop1 < ALUop2) ? 1 : 0;                   // Set less than (unsigned)
-        `ALUop_B:    ALUout = ALUop1;                                      // Pass-through (branch computation)
-        default:     ALUout = 0;                                           // Default case
+        `ALUop_ADD:  ALUout = ALUop1 + ALUop2;                             
+        `ALUop_SUB:  ALUout = ALUop1 - ALUop2;                             
+        `ALUop_AND:  ALUout = ALUop1 & ALUop2;                             
+        `ALUop_OR:   ALUout = ALUop1 | ALUop2;                             
+        `ALUop_XOR:  ALUout = ALUop1 ^ ALUop2;                            
+        `ALUop_LSL:  ALUout = ALUop1 << ALUop2;                       
+        `ALUop_LSR:  ALUout = ALUop1 >> ALUop2;                       
+        `ALUop_ASR:  ALUout = op1_signed >>> ALUop2;                  
+        `ALUop_SLT:  ALUout = (op1_signed < op2_signed) ? 1 : 0;           
+        `ALUop_SLTU: ALUout = (ALUop1 < ALUop2) ? 1 : 0;                   
+        `ALUop_B:    ALUout = ALUop2;                                      
+        default:     ALUout = 0;                                           
     endcase
 
-    // Zero flag for branch conditions
-    eq = (ALUout == 0) ? 1 : 0;
+    eq = (ALUout == 0'b0) ? 1 : 0;
 end
 
 endmodule
