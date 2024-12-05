6c39c0a7c5a5ccfafcdc3436352e3ecfccbb741f
selimenur <selimenurozyurt@gmail.com>
2024-12-05 14:29:20 +0000

    update i_mem

--- repo/rtl/i_mem.sv
+++ repo/rtl/i_mem.sv
@@ -1,19 +1,17 @@
 module i_mem #(
-    DATA_WIDTH = 32,
-    ADDRESS_WIDTH = 16
+    DATA_WIDTH = 32
 ) (
-    input   logic [ADDRESS_WIDTH-1:0]   pc,
+    input   logic [DATA_WIDTH-1:0]   pc,
     output  logic [DATA_WIDTH-1:0]      instr    
 );
-
-logic [7:0] array [0:2**ADDRESS_WIDTH-1]; // Byte-addressable memory
-
+ 
+logic [7:0] array [32'hBFC00FFF:32'hBFC00000]; // Byte-addressable memory
+ 
 initial begin
-		$display("Loading program into instruction memory...");
-		$readmemh("../rtl/program.hex", array);
+        $display("Loading program into instruction memory...");
+        $readmemh("../tb/program.hex", array);
 end
-
+ 
 assign instr = {array[pc+3], array[pc+2], array[pc+1], array[pc]};
-
+ 
 endmodule
-
