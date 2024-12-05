24ef1f246672c457c736b73ce82a7756b6287ab4
selimenur <selimenurozyurt@gmail.com>
2024-12-05 14:30:20 +0000

    update F-D pipe

--- repo/rtl/fetch_decode_pipe.sv
+++ repo/rtl/fetch_decode_pipe.sv
@@ -1,31 +1,34 @@
 module fetch_decode_pipe #(
-    parameter DATA_WIDTH = 16
+    parameter DATA_WIDTH = 32
 ) (
-    input clk,
-    input logic stallD,   //From Hazard Unit  acts as eable to FF
-    input logic flushD,   //From Hazard Unit  acts as rst to FF
-    input logic [DATA_WIDTH-1:0]  PCounterF,   // Before i_mem 
+    input                         clk,
+ 
+    input logic                   StallD,   //From Hazard Unit  acts as eable to FF
+    input logic                   FlushD,   //From Hazard Unit  acts as rst to FF
+ 
+    input logic [DATA_WIDTH-1:0]  pcF,   // Before i_mem
     input logic [DATA_WIDTH-1:0]  instr,       // output from i_mem
     input logic [DATA_WIDTH-1:0]  PCPlus4F,    // after +4
-
-    output logic [DATA_WIDTH-1:0] InstrD,      // after FF
-    output logic [DATA_WIDTH-1:0] PCounterD,   // after FF
-    output logic [DATA_WIDTH-1:0] PCPlus4D    //after FF 
-    // D -> after pipe, F -> before pipe 
+ 
+    output logic [DATA_WIDTH-1:0] instrD,      // after FF
+    output logic [DATA_WIDTH-1:0] pcD,   // after FF
+    output logic [DATA_WIDTH-1:0] PCPlus4D    //after FF
+    // D -> after pipe, F -> before pipe
 );
-
+ 
     always_ff @(posedge clk) begin
-
-        if (flushD)
-            InstrD <= 0 ;
-        
-        //Pass data through pipeline 
-        else if (!stallD) begin
-            InstrD <= instr;
-            PCounterD <= PCounterF;
+ 
+        //Pass data through pipeline
+        if (!StallD) begin
+            instrD <= instr;
+            pcD <= pcF;
             PCPlus4D <= PCPlus4F;      
         end
-
+ 
+        if (FlushD)
+            instrD <= 32'h00000013;
+    
+ 
     end
   
 endmodule
