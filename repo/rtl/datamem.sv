8956f2fd3665bd3d3d015b3e3d0c3c754c0d9df9
Alix Le Coguic <alix.le-coguic23@imperial.ac.uk>
2024-12-05 14:45:25 +0000

    updated datamem

--- repo/rtl/datamem.sv
+++ repo/rtl/datamem.sv
@@ -1,46 +1,45 @@
 module datamem #(
     parameter addr_width = 32,
-    parameter data_width = 32,
-    parameter memory_size = 128
+              data_width = 32,
+              memory_size = 8
 )(
     input logic clk,
-    input logic we, // write enable
-    input logic [2:0] funct3,
-    input logic [addr_width-1:0] daddr, // address
-    input logic [data_width-1:0] wd_data, // mem_write data
-    output logic [data_width-1:0] rd_data, // mem_read data
+    input logic we,
+    input logic [2:0] LS_mode,
+    input logic [addr_width-1:0] a,
+    input logic [data_width-1:0] wd,
+    output logic [data_width-1:0] rd
 );
-
-reg [data_width-1:0] mem [0:memory_size-1];
-logic [addr_width-1:0] add0, add1, add2, add3;
-
-initial 
-$readmemh("../rtl/program.hex", mem);
-
-assign add0 = (daddr & 32'hfffffffc)+ 32'h00000000;
-assign add1 = (daddr & 32'hfffffffc)+ 32'h00000001;s
-assign add2 = (daddr & 32'hfffffffc)+ 32'h00000002;
-assign add3 = (daddr & 32'hfffffffc)+ 32'h00000003;
-
-// Read logic with extension
-always_comb begin
-    case (funct3)
-        3'b010: rd_data = {mem[add3], mem[add2], mem[add1], mem[add0]};   // LW
-        3'b001: rd_data = {{16{mem[add1][7]}}, mem[add1], mem[add0]};   // LH (sign-extended)
-        3'b000: rd_data = {{24{mem[add0][7]}}, mem[add0]};            // LB (sign-extended)
-        3'b101: rd_data = {16'b0, mem[add1], mem[add0]};              // LHU (zero-extended)
-        3'b100: rd_data = {24'b0, mem[add0]};                       // LBU (zero-extended)
-        default: rd_data = 32'b0;
-    endcase
-end
-
-always_ff@(negedge clk) begin
-    if (we) begin
-        mem[add0] <= wd_data[7:0];
-        mem[add1] <= wd_data[15:8];
-        mem[add2] <= wd_data[23:16];
-        mem[add3] <= wd_data[31:24];
+ 
+    logic [memory_size-1:0] array [2**17-1:0];
+ 
+    initial begin
+        $display("Loading data into data memory...");
+        $readmemh("../tb/data.hex", array, 17'h10000, 17'h1FFFF);
     end
-end 
-
+ 
+// Read logic with extension
+    always_ff @* begin
+        rd = {
+            array[{a[16:2], 2'b11}],
+            array[{a[16:2], 2'b10}],
+            array[{a[16:2], 2'b01}],    
+            array[{a[16:2], 2'b00}]
+        };
+    end
+ 
+        // Read and write operations
+    always_ff @(posedge clk) begin
+        if (we && LS_mode == 3'b01x) begin // Write only least significant byte (8 bits)
+            array[a] <= wd[7:0];
+        end
+ 
+        else if (we) begin // Write whole word
+            array[{a[16:2], 2'b00}] <= wd[7:0];
+            array[{a[16:2], 2'b01}] <= wd[15:8];
+            array[{a[16:2], 2'b10}] <= wd[23:16];
+            array[{a[16:2], 2'b11}] <= wd[31:24];
+        end
+    end
+ 
 endmodule
